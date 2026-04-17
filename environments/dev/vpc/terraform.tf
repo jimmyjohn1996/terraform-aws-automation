96c89d1158aa250db3f6ab6d7d3cdd5bb9a0b27a
jimmyjohn1996 <jimmyjo8976@gmail.com>
2026-04-17 11:16:47 +0530

    Fix Terraform formatting

--- environments/dev/vpc/terraform.tf
+++ environments/dev/vpc/terraform.tf
@@ -23,10 +23,10 @@ provider "aws" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  environment            = var.environment
-  vpc_cidr              = var.vpc_cidr
-  public_subnet_cidrs   = var.public_subnet_cidrs
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  availability_zones    = var.availability_zones
-  cost_center           = var.cost_center
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  cost_center          = var.cost_center
 }
