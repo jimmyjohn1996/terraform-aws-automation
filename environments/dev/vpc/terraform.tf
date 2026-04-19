a5b0faf97580062e7b859f198b26b213460fc9fb
jimmyjohn1996 <jimmyjo8976@gmail.com>
2026-04-19 13:38:29 +0530

    removed private subnet fro tfvars

--- environments/dev/vpc/terraform.tf
+++ environments/dev/vpc/terraform.tf
@@ -26,7 +26,6 @@ module "vpc" {
   environment          = var.environment
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones   = var.availability_zones
   cost_center          = var.cost_center
 }
